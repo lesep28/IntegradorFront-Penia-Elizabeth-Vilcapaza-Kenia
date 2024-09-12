809d18a1f7288b398c71e3da415869a1fd11a37c
Kbri17 <104737155+Kbri17@users.noreply.github.com>
2024-09-11 22:46:19 -0500

    frontone

---
+++ js/listarOdontologos.js
@@ -0,0 +1,110 @@
+const apiURL = "http://localhost:8080";
+
+// Obtener la referencia a la tabla y al modal
+const tableBody = document.querySelector("#odontologoTable tbody");
+const editModal = new bootstrap.Modal(document.getElementById("editModal"));
+const editForm = document.getElementById("editForm");
+let currentOdontologoId;
+
+
+// Función para obtener y mostrar los odontólogos
+function fetchOdontologos() {
+  // listar los pacientes
+  fetch(`${apiURL}/odontologo/buscartodos`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Limpiar el contenido actual de la tabla
+      tableBody.innerHTML = "";
+
+      // Insertar los datos en la tabla
+      data.forEach((odontologo, index) => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+              <td>${odontologo.id}</td>
+              <td>${odontologo.apellido}</td>
+              <td>${odontologo.nombre}</td>
+              <td>${odontologo.nroMatricula}</td>
+              <td>
+                <button class="btn btn-primary btn-sm" onclick="editOdontologo(${odontologo.id}, '${odontologo.apellido}','${odontologo.nombre}', '${odontologo.nroMatricula}')">Modificar</button>
+                <button class="btn btn-danger btn-sm" onclick="deleteOdontologo(${odontologo.id})">Eliminar</button>
+              </td>
+            `;
+
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+// Función para abrir el modal y cargar los datos del Odontologo
+editOdontologo = function (
+  id,
+  apellido,
+  nombre,
+  nroMatricula
+) {
+  currentOdontologoId = id;
+  document.getElementById("editApellido").value = apellido;
+  document.getElementById("editNombre").value = nombre;
+  document.getElementById("editNroMatricula").value = nroMatricula;
+  editModal.show();
+};
+
+// Función para editar un odontologo
+editForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const apellido = document.getElementById("editApellido").value;
+  const nombre = document.getElementById("editNombre").value;
+  const nroMatricula = document.getElementById("editNroMatricula").value;
+
+  //modificar un odontologo
+  fetch(`${apiURL}/odontologo/modificar`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: currentOdontologoId,
+      nombre,
+      apellido,
+      nroMatricula,
+
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert("Odontologo modificado con éxito");
+      fetchOdontologos();
+      editModal.hide();
+    })
+    .catch((error) => {
+      console.error("Error editando odontologo:", error);
+    });
+});
+
+// Función para eliminar un odontologo
+deleteOdontologo = function (id) {
+  if (confirm("¿Está seguro de que desea eliminar este odontologo?")) {
+    // eliminar el odontologo
+    fetch(`${apiURL}/odontologo/eliminar/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        alert("Odontologo eliminado con éxito");
+        fetchOdontologos();
+      })
+      .catch((error) => {
+        console.error("Error borrando odontologo:", error);
+      });
+  }
+};
+
+// Llamar a la función para obtener y mostrar los odontólogos
+fetchOdontologos();
